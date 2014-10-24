9252225c653381acdec8d33d335440ace0e8e109
Arun Agrawal <arunagw@gmail.com>
2014-10-24 11:39:25 +0200

    Initial load

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'tinder'
+gem 'slack-notifier'
---
+++ stream.rb
@@ -0,0 +1,23 @@
+require 'tinder'
+require 'slack-notifier'
+
+notifier = Slack::Notifier.new ENV['SLACK_WEBHOOK_URL']
+
+campfire = Tinder::Campfire.new ENV['CAMPFIRE_ROOM'], :token => ENV['CAMPFIRE_TOKEN']
+
+room = campfire.rooms.last
+
+room.listen do |message|
+  user = message.user
+
+  if user
+    ping_data = {
+      username: "#{user.name} (Bot)",
+      icon_url: user.avatar_url
+    }
+  else
+    ping_data = {}
+  end
+
+  notifier.ping message.body.to_s, ping_data
+end
