6c720286931616537989d076626e8619bac41479
Arun Agrawal <arunagw@gmail.com>
2014-10-24 13:40:43 +0200

    lets not crash

--- stream.rb
+++ stream.rb
@@ -7,17 +7,21 @@ campfire = Tinder::Campfire.new ENV['CAMPFIRE_SUBDOMAIN'], :token => ENV['CAMPFI
 
 room = campfire.rooms.detect { |_room| _room.id.to_s == ENV['CAMPFIRE_ROOM'] }
 
-room.listen do |message|
-  user = message.user
+begin
+  room.listen do |message|
+    user = message.user
 
-  if user
-    ping_data = {
-      username: "#{user.name} (Bot)",
-      icon_url: user.avatar_url
-    }
-  else
-    ping_data = {}
+    if user
+      ping_data = {
+        username: "#{user.name} (Bot)",
+        icon_url: user.avatar_url
+      }
+    else
+      ping_data = {}
+    end
+
+    notifier.ping message.body.to_s, ping_data
   end
-
-  notifier.ping message.body.to_s, ping_data
+rescue => e
+  puts e.inspect
 end
